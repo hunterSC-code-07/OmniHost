76af119b4ec2abac6402caf89831e93c6964de6c
hunterSC-code-07 <hunterSC-code-07@users.noreply.github.com>
2026-08-28 13:23:03 +0530

    feat: add 7dtd frp tunneling ports

--- src/main/adapters/FrpAdapter.ts
+++ src/main/adapters/FrpAdapter.ts
@@ -157,6 +157,36 @@ localIP = "${localIp}"
 localPort = 8888
 remotePort = 8888
 `;
+    } else if (game === '7dtd') {
+      proxyConfig = `
+[[proxies]]
+name = "7dtd-26900-tcp"
+type = "tcp"
+localIP = "${localIp}"
+localPort = 26900
+remotePort = 26900
+
+[[proxies]]
+name = "7dtd-26900-udp"
+type = "udp"
+localIP = "${localIp}"
+localPort = 26900
+remotePort = 26900
+
+[[proxies]]
+name = "7dtd-26901-udp"
+type = "udp"
+localIP = "${localIp}"
+localPort = 26901
+remotePort = 26901
+
+[[proxies]]
+name = "7dtd-26902-udp"
+type = "udp"
+localIP = "${localIp}"
+localPort = 26902
+remotePort = 26902
+`;
     }
 
     const tomlConfig = `
